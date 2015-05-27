2a2cf82c9b51025094b124ce1234e5b1decb54f4
jcollyer <collyerdesign@gmail.com>
2015-05-27 07:14:11 -0700

    image loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,31 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.jsx$/, loader: 'jsx-loader' },
-      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' }
-    ]
+      {
+        test: /\.jsx$/, loader: 'jsx-loader'
+      }, {
+        test: /\.less$/, loader: 'style-loader!css-loader!less-loader'
+      }, {
+        test: /\.svg/,
+        loader: 'url-loader?limit=10000&minetype=image/svg+xml'
+      }, {
+        test: /\.eot/,
+        loader: 'url-loader?limit=10000&minetype=application/vnd.ms-fontobject'
+      }, {
+        test: /\.ttf|otf/,
+        loader: 'url-loader?limit=10000&minetype=application/font-sfnt'
+      }, {
+        test: /\.woff/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff'
+      }, {
+        test: /\.gif/,
+        loader: 'url-loader?limit=10000&minetype=image/gif'
+      }, {
+        test: /\.jpg/,
+        loader: 'url-loader?limit=10000&minetype=image/jpg'
+      }, {
+        test: /\.png/,
+        loader: 'url-loader?limit=10000&minetype=image/png'
+    }]
   }
 };
