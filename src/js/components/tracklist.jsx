f3fbef78907ecb2b2c421c7ca98d5b77d995fbeb
jcollyer <collyerdesign@gmail.com>
2015-05-27 21:12:25 -0700

    only get tracks with images, substitute larger track artwork

--- src/js/components/tracklist.jsx
+++ src/js/components/tracklist.jsx
@@ -8,12 +8,20 @@ TrackList =
       return {tracks: []};
     },
     getTracks: function(genre) {
+      that = this;
+      var goodTracks = [];
       var url = 'http://api.soundcloud.com/tracks?'+genre+'&client_id=51b52c948e268a19b58f87f3d47861ad';
+      // var url = 'https://api.soundcloud.com/explore/sounds/category?rock&limit=L&client_id=51b52c948e268a19b58f87f3d47861ad';
       $.ajax({
         url: url,
         dataType: 'json',
         success: function(tracks) {
-          this.setState({tracks: tracks});
+          tracks.forEach(function(track){
+            if (track.artwork_url != null){
+              goodTracks.push(track);
+            }
+            that.setState({tracks: goodTracks});
+          });
         }.bind(this),
         error: function(xhr, status, err) {
           console.error(xhr, status, err.toString());
@@ -27,11 +35,12 @@ TrackList =
       return (
         <div>
           {this.state.tracks.map(function(track){
+            var bigImage = track.artwork_url? track.artwork_url.replace('large', 't200x200') : "";
             return (
               <div className='col-md-3'>
                 <Track
                       title={track.title}
-                      artwork={track.artwork_url}
+                      artwork={bigImage}
                       id={track.id}
                       duration={track.duration}
                       author={track.user.username}
