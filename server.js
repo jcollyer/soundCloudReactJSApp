7e67bc38615f17dedbe316de823212c93961353c
Jeremy Collyer <collyerdesign@gmail.com>
2015-06-27 16:04:10 -0700

    update port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, '0.0.0.0', function (err, result) {
+}).listen(process.env.PORT || 3000, '0.0.0.0', function (err, result) {
   if (err) console.log(err);
   console.log('Listening at 0.0.0.0:3000');
 });
