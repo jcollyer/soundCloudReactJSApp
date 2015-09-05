29c5853cfa39d9481b0c92610183ce931353d363
jcollyer <collyerdesign@gmail.com>
2015-09-05 09:47:21 -0700

    test a

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 var Webpack = require('webpack');
+var path = require('path');
+var mainPath = path.resolve(__dirname, 'src', 'main.jsx');
 
 module.exports = {
   devtool: 'eval',
-  entry: [
-    './src/main.jsx'
-  ],
+  entry: mainPath,
   output: {
     path: '/build',
     filename: 'bundle.js'
