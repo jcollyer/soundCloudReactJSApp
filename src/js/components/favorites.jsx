9ccb6fb321aaf8ccb4e458140a057e0a8b1eee0e
jcollyer <collyerdesign@gmail.com>
2015-09-09 07:23:22 -0700

    add delete favorite to favorite pannel

--- src/js/components/favorites.jsx
+++ src/js/components/favorites.jsx
@@ -32,10 +32,35 @@ var Favorites = React.createClass({
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
   },
+  removeTrack: function(trackId) {
+    var userId = AppStore.getUserId();
+    var trackId = trackId;
+    var path = 'https://api.soundcloud.com/users/'+userId+'/favorites/'+trackId+'?client_id=b5e21578d92314bc753b90ea7c971c1e';
+
+    SC.delete(path, function(response, error) {
+      if (error) {
+        console.log("Some error occured: " + error);
+      } else {
+        console.log("favorite deleted");
+        var favorite = document.getElementById(trackId);
+        favorite.classList.add("remove_track");
+      }
+    });
+  },
+  deleteTrack: function(trackId) {
+    var id = id;
+    var isLoggedIn = AppStore.isLoggedIn();
+    if(isLoggedIn) {
+      this.removeTrack(trackId);
+    } else {
+      login();
+    }
+  },
   closeFavoritePane: function() {
     document.getElementById('favorites-wrapper').classList.add('close');
   },
   render: function() {
+    var that = this;
     return (
       <div>
         <button onClick={this.getUserFavorites}>My Favorites</button>
@@ -43,7 +68,7 @@ var Favorites = React.createClass({
           <button onClick={this.closeFavoritePane}>X</button>
           {this.state.favorites.map(function(track){
             return (
-              <div className='col-md-12 favorite' key={track.id}>
+              <div className='col-md-12 favorite-track-wrapper' key={track.id} id={track.id}>
                 <Track
                       title={track.title}
                       artwork={track.artwork_url}
@@ -51,6 +76,9 @@ var Favorites = React.createClass({
                       duration={track.duration}
                       author={track.user.username}
                 />
+                <div className="favorite-actions">
+                  <button className="track-delete" onClick={that.deleteTrack.bind(null, track.id)}>Delete</button>
+                </div>
               </div>
             )
           })}
