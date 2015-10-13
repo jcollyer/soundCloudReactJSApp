b6d657adbed19faaaa8b7c9564cbfd09fb892b87
jcollyer <collyerdesign@gmail.com>
2015-10-13 08:01:25 -0700

    start implementing proper get playlists

--- src/js/actions/playlists-actions.js
+++ src/js/actions/playlists-actions.js
@@ -1,13 +1,13 @@
 // var PlaylistsConstants = require('../constants/playlists-constants.js');
 // var PlaylistsDispatcher = require('../dispatchers/playlists-dispatcher.js');
-//
+
 // var PlaylistsActions = {
-//   // getPlaylists:function(userId){
-//   //   PlaylistsDispatcher.handlePlaylistsAction({
-//   //     actionType: PlaylistsConstants.GET_FAVORITES,
-//   //     userId: userId
-//   //   })
-//   // },
+//   getPlaylists:function(userId){
+//     PlaylistsDispatcher.handlePlaylistsAction({
+//       actionType: PlaylistsConstants.GET_PLAYLISTS,
+//       userId: userId
+//     })
+//   },
 //   openPlaylists:function(userAction){
 //     PlaylistsDispatcher.handlePlaylistsAction({
 //       actionType: PlaylistsConstants.SET_PLAYLISTS,
