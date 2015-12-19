1fce71b8ce8fdbc7eb44a2da00e3d7b3f828f1a4
jeremy collyer <jcollyer@gopro.com>
2015-12-18 16:34:22 -0800

    home style update

--- src/js/components/home.jsx
+++ src/js/components/home.jsx
@@ -6,7 +6,7 @@ var Home =
     render: function() {
       return (
         <div id="home-page">
-          <h1>Home!</h1>
+          <h1>Click to get started...</h1>
           <Genre />
         </div>
       );
