b5a293ec42be15472f9324fc19cc36ef0c362a1d
jeremy collyer <jcollyer@gopro.com>
2016-02-11 15:24:39 -0500

    update var

--- src/main.jsx
+++ src/main.jsx
@@ -4,4 +4,4 @@ var App = require('./App.jsx');
 
 require('./sc-player.jsx');
 
-App = ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
